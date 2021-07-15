5219151295a43fb1c1441150715af5e419b7581a
lnm011223 <2969827073@qq.com>
2021-07-15 16:04:37 +0800

    first

--- src/com/company/Welcome.java
+++ src/com/company/Welcome.java
@@ -1,2 +1,12 @@
-package com.company;public class Welcome {
+package com.company;
+
+public class Welcome {
+    public static void main(String[] args){
+        String greeting = "Welcome to Core Java";
+        
+        for (int i=0;i < greeting.length();i++){
+            System.out.println(greeting);
+
+        }
+    }
 }
