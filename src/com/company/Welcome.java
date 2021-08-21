00326a69938581632bbfde80b4f4398e8098464d
lnm011223 <2969827073@qq.com>
2021-08-22 00:12:35 +0800

    2021-8-21

--- src/com/company/Welcome.java
+++ src/com/company/Welcome.java
@@ -1,11 +1,12 @@
 package com.company;
 
+import java.util.Date;
+
 public class Welcome {
     public static void main(String[] args){
-        String greeting = "Welcome to Core Java";
-        
-        for (int i=0;i < greeting.length();i++){
-            System.out.println(greeting);
+        String s = new Date().toString();
+        for (int i=0;i < 10 ;i++){
+            System.out.println(s);
 
         }
     }
---
+++ src/com/company/datenow.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class datenow {
+    public static void main(String[] args){
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+
+        date = date.minusDays(today-1);
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+
+        System.out.println("mon tue wed thu fri sat sun");
+        for (int i=1;i<value;i++){
+            System.out.print("    ");
+
+        }
+        while (date.getMonthValue()==month){
+            System.out.printf("%3d",date.getDayOfMonth());
+            if (date.getDayOfMonth()==today) {
+                System.out.print("*");
+            }
+            else
+                System.out.print(" ");
+            date=date.plusDays(1);
+            if (date.getDayOfWeek().getValue()==1) System.out.println();
+            }
+        if (date.getDayOfWeek().getValue()!=1) System.out.println();
+
+    }
+
+
+}
